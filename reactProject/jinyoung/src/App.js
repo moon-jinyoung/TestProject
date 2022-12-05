235aedb529e0705d4814def8b6ef9aefa0322304
MoonJinYoung <ans8181@naver.com>
2022-12-05 16:53:09 +0900

    221205

--- reactProject/jinyoung/src/App.js
+++ reactProject/jinyoung/src/App.js
@@ -1,100 +1,31 @@
-import { useState, useEffect, useRef } from "react";
-import './App.css';
-import Dots from "./Dots"; // 우측 페이지 표시
+import React from "react";
+import styled from "styled-components";
+import Content from "./Content";
 
-const DIVIDER_HEIGHT = 5;
-
-function App() {
-  const outerDivRef = useRef();
-  const [scrollIndex, setScrollIndex] = useState(1);
-
-  useEffect(() => {
-    const wheelHandler = (e) => {
-      e.preventDefault();
-      const { deltaY } = e;
-      const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
-      const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
-      
-      if (deltaY > 0) {
-        // 스크롤 내릴 때
-        if (scrollTop >= 0 && scrollTop < pageHeight) {
-          //현재 1페이지
-          console.log("현재 1페이지, down");
-          outerDivRef.current.scrollTo({
-            top: pageHeight + DIVIDER_HEIGHT,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(2); // 동작(아래로)후 2페이지
-        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
-          //현재 2페이지
-          console.log("현재 2페이지, down");
-          outerDivRef.current.scrollTo({
-            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(3); // 동작(아래로)후 3페이지
-        } else {
-          // 현재 3페이지
-          console.log("현재 3페이지, down");
-          outerDivRef.current.scrollTo({
-            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
-            left: 0,
-            behavior: "smooth",
-          });
-          
-        setScrollIndex(3); // 동작(아래로)후 3페이지
-        }
-      } else {
-        // 스크롤 올릴 때
-        if (scrollTop >= 0 && scrollTop < pageHeight) {
-          //현재 1페이지
-          console.log("현재 1페이지, up");
-          outerDivRef.current.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(1); // 동작(위로)후 1페이지
-        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
-          //현재 2페이지
-          console.log("현재 2페이지, up");
-          outerDivRef.current.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(1); // 동작(위로)후 1페이지
-        } else {
-          // 현재 3페이지
-          console.log("현재 3페이지, up");
-          outerDivRef.current.scrollTo({
-            top: pageHeight + DIVIDER_HEIGHT,
-            left: 0,
-            behavior: "smooth",
-          });
-          setScrollIndex(2); // 동작(위로)후 2페이지
-        }
-      }
-    };
-    const outerDivRefCurrent = outerDivRef.current;
-    outerDivRefCurrent.addEventListener("wheel", wheelHandler);
-    return () => {
-      outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
-    };
-  }, []);
-
-  return (
-    <div ref={outerDivRef} className="outer">
-      <Dots scrollIndex={scrollIndex} />
-      <div className="inner bg-yellow">1</div>
-      <div className="divider"></div>
-      <div className="inner bg-blue">2</div>
-      <div className="divider"></div>
-      <div className="inner bg-pink">3</div>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+    <Container>
+      <Content />
+    </Container>
+    );
+  }
 }
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+      to right,
+      rgba(20, 20, 20, 0.1) 10%,
+      rgba(20, 20, 20, 0.7) 70%,
+      rgba(20, 20, 20, 1)
+    ),
+    url(https://source.unsplash.com/random/1920x1080);
+  background-size: cover;
+  `;
 
 export default App;
